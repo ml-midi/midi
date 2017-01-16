dc0062cf209c900302cc829b1eae3f7ff6077750
Lars Lubkoll <lars.lubkoll@posteo.de>
2017-01-16 22:23:51 +0100

    Add cmake-directory

---
+++ cmake/SpacyMusicConfig.cmake
@@ -0,0 +1,8 @@
+# - Config file for the SpacyMusic package 
+set(SpacyMusic_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
+set(SpacyMusic_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libspacy_music.a)
+set(SpacyMusic_LIBRARY_DIR ${CMAKE_INSTALL_PREFIX}/lib)
+set(SpacyMusic_LIBS spacy_music)
+set(SpacyMusic_CXX_FLAGS "-std=c++1y")
+set(SpacyMusic_CXX_DEFINITIONS "-std=c++1y")
+set(SpacyMusic_CXX_STANDARD 14)
